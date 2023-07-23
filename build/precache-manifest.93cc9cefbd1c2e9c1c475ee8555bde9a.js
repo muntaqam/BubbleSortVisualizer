d9cac54195179aa1fb9ad8a289df849226e7e108
muntaqam <maahi305@yahoo.com>
2023-07-22 20:55:43 -0400

    adding build

---
+++ build/precache-manifest.93cc9cefbd1c2e9c1c475ee8555bde9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7962a66455c091157ea8",
+    "url": "/static/css/main.df566e45.chunk.css"
+  },
+  {
+    "revision": "7962a66455c091157ea8",
+    "url": "/static/js/main.7962a664.chunk.js"
+  },
+  {
+    "revision": "4c9aa3f9f967913d2422",
+    "url": "/static/js/1.4c9aa3f9.chunk.js"
+  },
+  {
+    "revision": "6cb367649b869b064cb9",
+    "url": "/static/js/2.6cb36764.chunk.js"
+  },
+  {
+    "revision": "981709b59cce671627b4",
+    "url": "/static/js/runtime~main.981709b5.js"
+  },
+  {
+    "revision": "fd021770dfd8e496ada95c582fbc9287",
+    "url": "/index.html"
+  }
+];
